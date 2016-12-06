d92c70ae747328ac9203ef2b793687b8c85e4637
Lizzie Shipton <lizzie.shipton@gmail.com>
2016-12-05 17:50:45 -0800

    Testing Database

--- tuts/settings.py
+++ tuts/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -72,7 +73,6 @@ WSGI_APPLICATION = 'tuts.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -80,6 +80,9 @@ DATABASES = {
     }
 }
 
+db_config = dj_database_url.config()
+if db_config:
+ DATABASES['default'] =  db_config
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
